8a230e57ee01fa39d9fde8c4cb1626aaf6122a4b
Sanket-Makkar <114882219+Sanket-Makkar@users.noreply.github.com>
2023-05-22 12:32:04 -0400

    Create up_down_counter.sv

---
+++ up_down_counter.sv
@@ -0,0 +1,25 @@
+module up_down_counter #(
+    parameter N = 4
+) (
+    input  logic         clk,
+    input  logic         en_b,
+    input  logic         load_b,
+    input  logic         up,
+    input  logic [N-1:0] load_in,
+    output logic [N-1:0] q,
+    output logic         rco_b    
+);
+    always_ff @(posedge clk) begin
+        if (~en_b) begin
+            if (load_b) begin
+                q <= up ? (q + 4'b0001):(q - 4'b0001);              
+            end
+            else begin
+                q <= load_in;
+            end
+        end
+    end
+
+    assign rco_b = ~(&(q));
+
+endmodule
